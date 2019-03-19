bb8295f605c478afcc821941cd21ab4b52937074
Carita Logrén <carita.logren@gmail.com>
2019-03-19 08:33:09 +0200

    Remove unused spring framework resource import.

--- src/main/java/com/rest/szz/controller/AppController.java
+++ src/main/java/com/rest/szz/controller/AppController.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ServerProperties.Tomcat.Resource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
