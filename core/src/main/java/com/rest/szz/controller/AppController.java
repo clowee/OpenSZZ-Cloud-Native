a6a93fc46f64f8b10211b778b5abfc6d0824412c
linnat <linnat@student.tut.fi>
2019-03-20 10:59:29 +0200

    removed unused import ..Async

--- core/src/main/java/com/rest/szz/controller/AppController.java
+++ core/src/main/java/com/rest/szz/controller/AppController.java
@@ -17,7 +17,6 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
