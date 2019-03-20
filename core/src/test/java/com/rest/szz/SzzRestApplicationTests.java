f0d5a7a53a25783f069cc0a18305a9dce1254809
jatobedi <32989632+jatobedi@users.noreply.github.com>
2019-03-20 15:08:20 +0200

    removed spring runner import (#17)

--- core/src/test/java/com/rest/szz/SzzRestApplicationTests.java
+++ core/src/test/java/com/rest/szz/SzzRestApplicationTests.java
@@ -32,7 +32,6 @@ import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
