1f512b2d4647d49b422f22211c7a79b2158ab843
Timo Närhi <narhitt@gmail.com>
2019-03-20 14:30:22 +0200

    removed duplicate import for junit4 from line 56

--- core/src/test/java/com/rest/szz/SzzRestApplicationTests.java
+++ core/src/test/java/com/rest/szz/SzzRestApplicationTests.java
@@ -53,7 +53,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.restdocs.constraints.ConstraintDescriptions;
-import org.springframework.test.context.junit4.SpringRunner;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
