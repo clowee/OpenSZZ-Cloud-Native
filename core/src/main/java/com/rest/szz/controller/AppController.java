a17e4d650ced00351eb87edd3a5387712baf82c1
Juho-Pekka Mattila <juho-pekka.mattila@student.tut.fi>
2019-03-21 10:43:48 +0200

    Did task: Remove this unused import 'com.rest.szz.git.JiraRetriever'.

--- core/src/main/java/com/rest/szz/controller/AppController.java
+++ core/src/main/java/com/rest/szz/controller/AppController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rest.szz.git.Application;
-import com.rest.szz.git.JiraRetriever;
 import com.rest.szz.helpers.Email;
 
 @RestController
