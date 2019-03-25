83babc526dbf6f84546d0e5a3a5bd389734ca2a7
luca9294 <luca9294@hotmail.it>
2019-03-25 22:46:38 +0100

    New pages

--- gui/src/main/java/org/openszzweb/samples/app/system/WelcomeController.java
+++ gui/src/main/java/org/openszzweb/samples/app/system/WelcomeController.java
@@ -1,6 +1,8 @@
 package org.openszzweb.samples.app.system;
 
 
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -19,7 +21,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 class WelcomeController {
 	
 	 private final RabbitTemplate rabbitTemplate;
-	 
+     private String jiraAPI = "/jira/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml";
+
 	 private static final String topicExchangeSzz = "szz-analysis-exchange";
 	 private static final String queueNameSzz = "szz-analysis";
 	
@@ -34,16 +37,40 @@ class WelcomeController {
     }
     
 	@PostMapping("/doAnalysis")
-    public boolean doAnalysis(@ModelAttribute Analysis analysis,BindingResult result, Model model) {
+    public String doAnalysis(@ModelAttribute Analysis analysis,BindingResult result, Model model) {
 	       List<String> t = new LinkedList<String>();
 	        t.add(analysis.getGitUrl());
 	        t.add(analysis.getJiraUrl());
 	        t.add(analysis.getEmail());
 	        rabbitTemplate.convertAndSend("szz-analysis-exchange", "project.name."+analysis.getProjectName(), t);
-	        return true;
+	        if (!checkCorrectness(analysis))
+	        		return "error";
+	        model.addAttribute(analysis);
+	        return "resultPage";
     }
 	
 	private boolean checkCorrectness(Analysis analysis){
+		//Checks url github
+		try {
+		    URL url = new URL(analysis.getGitUrl());
+		    URLConnection conn = url.openConnection();
+		    conn.connect();
+		} catch (Exception e) {
+		    return false;
+		} 
+		//Checks url github
+		try {
+			String[] array = analysis.getJiraUrl().split("/jira/projects/");
+			String projectName = array[1].replaceAll("/", "");
+			String jiraUrl = array[0] + jiraAPI;
+		    URL url = new URL(jiraUrl);
+		    URLConnection conn = url.openConnection();
+		    conn.connect();
+		} catch (Exception e) {
+		    return false;
+		} 
+		
+	
 		return true;
 	}
 }
