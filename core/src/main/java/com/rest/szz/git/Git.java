bd23e1543510fe87ddf79d00a238609192e75d1a
Vladyslav Bondarenko <vladyslav.bondarenko@ut.ee>
2021-02-28 18:13:20 +0200

    avoid cloning repository if already exists

--- core/src/main/java/com/rest/szz/git/Git.java
+++ core/src/main/java/com/rest/szz/git/Git.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.ProcessBuilder.Redirect;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -88,7 +89,9 @@ public class Git {
 	}
 
 	public void cloneRepository() throws Exception {
-		execute(this.cloneCommand, this.storagePath);
+		if (!Files.exists(workingDirectory.toPath())) {
+			execute(this.cloneCommand, this.storagePath);
+		}
 	}
 
 	public void pullUpdates() throws Exception {
